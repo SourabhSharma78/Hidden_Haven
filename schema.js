0935898178662fc3fa91c912f2f126d7817a9d47
Sourabh Sharma <112540353+SourabhSharma78@users.noreply.github.com>
2024-11-29 13:48:16 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -6,6 +6,7 @@ module.exports.listingSchema = Joi.object({
         description : Joi.string().required(),
         image : Joi.string().allow("",null),
         price : Joi.number().required().min(0),
+        sports : Joi.string(),
         location : Joi.string().required(),
         country : Joi.string().required(),
     }).required(),
